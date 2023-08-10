a11447ccfbac1ca916081c70c91864c567cdde1a
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-10 23:53:33 +0200

    practitionners card error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import Card from '../../components/Card';
 
 export default function TabOneScreen() {
   return (
@@ -9,6 +10,16 @@ export default function TabOneScreen() {
       <View style={{flexDirection:'row', marginTop:20, justifyContent:'space-between', alignItems:'baseline', paddingHorizontal:16}}>
           <Text style={{color:'#415556', fontSize:20}}>Your Practitioners</Text>
           <Text style={{color:'#29AAC9', fontSize:14, opacity:0.9}}>See More</Text>
+      </View>
+      <View style={styles.practitionersContainer}>
+          <ScrollView horizontal={true} contentContainerStyle={{columnGap: 16, paddingLeft:16, paddingRight:16, paddingBottom:20}}>
+            <Card text={'aaa'} firstname="Connie" avatarUri={require('../../assets/avatars/connie_avatar.png')}/>
+            <Card text={'bbb'} firstname="Olga" avatarUri={require('../../assets/avatars/olga_avatar.png')}/>
+            <Card text={'ccc'} firstname="Usman" avatarUri={require('../../assets/avatars/usman_avatar.png')}/>
+            <Card text={'ddd'} firstname="Henry" avatarUri={require('../../assets/avatars/henry_avatar.png')}/>
+            <Card text={'eee'} firstname="Masud" avatarUri={require('../../assets/avatars/masud_avatar.png')}/>
+            <Card text={'ddd'} firstname="Olga" avatarUri={require('../../assets/avatars/olga_avatar.png')}/>
+          </ScrollView>
       </View>
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
@@ -19,6 +30,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection:'column',
+  },
+  practitionersContainer:{
+    height:100,
+    marginTop:10,
   },
   /*title: {
     fontSize: 20,
---
+++ components/Card.tsx
@@ -0,0 +1,41 @@
+import { Image, ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
+
+type Props = {
+    text : string,
+    avatarUri : ImageSourcePropType
+    firstname : string
+}
+
+// const connie = require('../assets/avatars/connie_avatar.png')
+
+const Card = (props: Props) => {
+
+  return (
+    <View style={styles.cardContainer}>
+      <View style={styles.avatarContainer}>
+        <Image style={[styles.avatar, {zIndex:3}]} source={props.avatarUri}/>
+        <Image style={styles.avatar} blurRadius={1} source={props.avatarUri}/>
+      </View>
+      <Text style={{textAlign:'center', fontSize:12, color:'#374B4C'}}>{props.firstname}</Text>
+    </View>
+  )
+}
+
+export default Card
+
+const styles = StyleSheet.create({
+    cardContainer :{
+      flexDirection:'column',
+      rowGap:4,
+      width:66,
+      height:100,
+    },
+    avatarContainer : {height:66,width:66, backgroundColor:'#fff', padding:20, borderRadius:12, fontFamily: 'Montserrat_500Medium',
+     border: '2px solid #fff',
+     backgroundImage: `linear-gradient(0deg, rgba(55, 216, 255, 0.30) 0%, rgba(55, 216, 255, 0.30) 100%), 
+     linear-gradient(180deg, rgba(138, 221, 239, 0.50) 0%, rgba(0, 127, 134, 0.50) 100%)`,
+     overflow:'hidden',
+    },
+    avatar : {position:'absolute',height:'100%',width:'100%', top:0, left:0,
+    },
+})
