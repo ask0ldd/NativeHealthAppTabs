00083e91e5eb4372ccdcfc0dd611ed125605ac6a
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-12 01:37:44 +0200

    appointment date container

--- app/appointment/index.tsx
+++ app/appointment/index.tsx
@@ -14,24 +14,37 @@ export default function AppointmentScreen() {
       <View style={styles.buttonsnCardContainer}>
         <View style={styles.buttonsContainer}>
             <Pressable style={styles.buttons}></Pressable>
-            <Pressable style={[styles.buttons, {marginTop:'auto'}]}></Pressable>
-            <Pressable style={styles.buttons}></Pressable>
+            <Pressable style={[styles.blueButtons, {marginTop:'auto'}]}></Pressable>
+            <Pressable style={styles.blueButtons}></Pressable>
         </View>
         <LinearGradient colors={['#FCFEFE', '#E2F5F9']} style={styles.card}>
             <Image style={{position: 'absolute', left: '35%', top:0 }} source={require('../../assets/heartbgpattern.png')} />
             <View style={{position:'absolute', borderColor:'#10A5C7', borderWidth:1, borderStyle:'dashed', top:'50%', width:'100%', opacity:0.3}}/>
             <Image style={{opacity: 0.93, position: 'absolute', alignSelf: 'flex-end', right:16, bottom:0}} source={require('../../assets/avatars/oliver_avatar.png')}/>
-            <Text style={{color: '#374B4C', fontSize: 16, marginTop: 16}}>Dr Oliver SYKES</Text>
-            <Text style={{color: 'rgba(137, 164, 171, 0.9)', fontSize: 12, marginTop: 6}}>HEAR SURGEON</Text>
-            <View style={{flexDirection: 'row', alignItems: 'center', columnGap: 8, marginTop: 9, backgroundColor:'#00000000'}}>
+            <Text style={{color: '#374B4C', fontSize: 16, marginTop: 16, marginLeft:18}}>Dr Oliver SYKES</Text>
+            <Text style={{color: 'rgba(137, 164, 171, 0.9)', fontSize: 12, marginTop: 4, marginLeft:18}}>HEAR SURGEON</Text>
+            <View style={{flexDirection: 'row', alignItems: 'center', columnGap: 8, marginTop: 14, backgroundColor:'#00000000', marginLeft:18}}>
                 <Ratings rating={5}/>
                 <Text style={{color: 'rgba(137, 164, 171, 0.9)', fontSize: 12}}>(22 Reviews)</Text>
             </View>
-            <Image style={{marginTop:32}} source={require('../../assets/mayo_blason.png')}/>
-            <View style={{flexDirection:'row', backgroundColor:'#00000000'}}>
+            <Image style={{marginTop:31, marginLeft:18}} source={require('../../assets/mayo_blason.png')}/>
+            <View style={{flexDirection:'row', backgroundColor:'#00000000', marginTop:14, marginLeft:18, alignItems:'center'}}>
                 <Image source={require('../../assets/icons/localisation_icon.png')}/>
+                <View style={{flexDirection:'column', marginLeft:16, backgroundColor:'#00000000'}}>
+                    <Text style={{fontSize:10, color:'#89A4AB'}}>9500 Euclid Avenue,</Text>
+                    <Text style={{fontSize:10, color:'#89A4AB'}}>Cleveland, OH</Text>
+                </View>
             </View>
         </LinearGradient>
+      </View>
+      <View style={{flexDirection:'row', marginTop:10, justifyContent:'space-between', alignItems:'baseline', paddingHorizontal:16, backgroundColor:'#00000000'}}>
+          <Text style={{color:'#415556', fontSize:20}}>Make an Appointment</Text>
+      </View>
+      <View style={{flexDirection:'row', columnGap:18, height:72, backgroundColor:'#00000000', marginTop:20, paddingHorizontal:16}}>
+        <Pressable style={styles.activeDateButton}>
+            <LinearGradient style={{width:54, height:72, justifyContent:'center', alignItems:'center'}} colors={['#ffffff44', 'transparent']} start={{x:1, y:0}} end={{x:0.2, y:0.8}}>
+            </LinearGradient>
+        </Pressable>
       </View>
     </LinearGradient>
   );
@@ -62,7 +75,7 @@ const styles = StyleSheet.create({
     shadowColor:'#23BEE3',
     elevation:12,
     shadowOpacity:1,
-    paddingLeft:18,
+    //paddingLeft:18,
   },
   buttonsContainer:{
     height:200,
@@ -82,5 +95,30 @@ const styles = StyleSheet.create({
     elevation:4,
     shadowOpacity:1,
   },
+  blueButtons:{
+    height:42,
+    width:42,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:10,
+    elevation:10,
+    shadowOpacity:1,
+    backgroundColor:'rgba(35, 190, 227, 0.6)',
+    borderWidth:1,
+    borderColor:'#fff',
+    shadowColor:'#39C5E6ff',
+  },
+  activeDateButton:{
+    height:72,
+    width:54,
+    borderRadius:6,
+    backgroundColor:'rgba(35, 190, 227, 0.6)',
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth:1,
+    borderColor:'#fff',
+    shadowColor:'#39C5E6ff',
+    elevation:10,
+}
 
 });
