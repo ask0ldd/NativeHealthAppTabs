f4ad543565dce063d0778cacdd8107bc6acb91ca
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-11 23:03:09 +0200

    appointment all top containers

--- app/appointment/index.tsx
+++ app/appointment/index.tsx
@@ -9,9 +9,13 @@ export default function AppointmentScreen() {
 
   return (
     <LinearGradient colors={['#B9EFF3','#EDF5F7']} style={styles.container}>
-      <Image style={{position:'absolute', top:90, left:-120,}} source={require('../../assets/bgpattern.png')}/>
+      <Image style={{position:'absolute', top:110, left:-120,}} source={require('../../assets/bgpattern.png')}/>
       <View style={styles.buttonsnCardContainer}>
-        <View style={styles.buttonsContainer}></View>
+        <View style={styles.buttonsContainer}>
+            <Pressable style={styles.buttons}></Pressable>
+            <Pressable style={[styles.buttons, {marginTop:'auto'}]}></Pressable>
+            <Pressable style={styles.buttons}></Pressable>
+        </View>
         <LinearGradient colors={['#FCFEFE', '#E2F5F9']} style={styles.card}>
 
         </LinearGradient>
@@ -27,12 +31,14 @@ const styles = StyleSheet.create({
   },
   buttonsnCardContainer: {
     flexDirection:'row',
-    height:200,
+    height:250,
     width:'100%',
-    columnGap:18,
+    columnGap:16,
     paddingHorizontal:16,
     backgroundColor:'#00000000',
     paddingTop:20,
+    paddingBottom:30,
+    overflow:'visible',
   },
   card:{
     flex:1,
@@ -48,8 +54,19 @@ const styles = StyleSheet.create({
     height:200,
     width:42,
     flexDirection:'column',
-    rowGap:18,
+    rowGap:16,
     backgroundColor:'#00000000',
+  },
+  buttons:{
+    height:42,
+    width:42,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#FFFFFFFF',
+    borderRadius:10,
+    shadowColor:'#23BEE3',
+    elevation:4,
+    shadowOpacity:1,
   },
 
 });
