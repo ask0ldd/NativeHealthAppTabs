adfaaf005bfc548a3c18b397e2a8aaef038119c8
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-13 21:46:14 +0200

    keyboardawarescrollview added

--- app/appointment/index.tsx
+++ app/appointment/index.tsx
@@ -1,4 +1,4 @@
-import { Image, ScrollView, StyleSheet, Pressable, TextInput, Dimensions, KeyboardAvoidingView } from 'react-native';
+import { Image, ScrollView, StyleSheet, Pressable, TextInput, Dimensions} from 'react-native';
 
 import { Text, View } from '../../components/Themed';
 import { LinearGradient } from 'expo-linear-gradient'
@@ -8,6 +8,8 @@ import Ratings from '../../components/Ratings';
 import DateButton from '../../components/DateButton';
 import TimeSlotButton from '../../components/TimeSlotButton';
 import { BorderlessButton } from 'react-native-gesture-handler';
+
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const Timeslots : Array<string> = ["09h00 AM", "10h00 AM", "11h00 AM", "13h00 PM", "14h00 PM", "15h00 PM", "16h00 PM", "17h00 PM", "18h00 PM"]
 
@@ -25,101 +27,101 @@ export default function AppointmentScreen() {
   const [ bookingMessage, setBookingMessage ] = useState<string>()
 
   return (
-    <ScrollView>
-    <LinearGradient colors={['#B9EFF3','#EDF5F7']} style={styles.container}>
-      <Image style={{position:'absolute', top:110, left:-120,}} source={require('../../assets/bgpattern.png')}/>
-      <View style={styles.buttonsnCardContainer}>
-        <View style={styles.buttonsContainer}>
-            <Pressable style={styles.buttons}></Pressable>
-            <View style={[styles.blueButtons, {marginTop:'auto'}]}>
-              <LinearGradient style={{width:42, height:42, justifyContent:'center', alignItems:'center'}} colors={['#ffffff44', 'transparent']} start={{x:1, y:0}} end={{x:0.2, y:0.8}}>
-                <Image source={require('../../assets/icons/phone.png')}/>
+    <KeyboardAwareScrollView enableOnAndroid>
+      <LinearGradient colors={['#B9EFF3','#EDF5F7']} style={styles.container}>
+        <Image style={{position:'absolute', top:110, left:-120,}} source={require('../../assets/bgpattern.png')}/>
+        <View style={styles.buttonsnCardContainer}>
+          <View style={styles.buttonsContainer}>
+              <Pressable style={styles.buttons}></Pressable>
+              <View style={[styles.blueButtons, {marginTop:'auto'}]}>
+                <LinearGradient style={{width:42, height:42, justifyContent:'center', alignItems:'center'}} colors={['#ffffff44', 'transparent']} start={{x:1, y:0}} end={{x:0.2, y:0.8}}>
+                  <Image source={require('../../assets/icons/phone.png')}/>
+                </LinearGradient>
+              </View>
+              <Pressable style={styles.blueButtons}>
+                <LinearGradient style={{width:42, height:42, justifyContent:'center', alignItems:'center'}} colors={['#ffffff44', 'transparent']} start={{x:1, y:0}} end={{x:0.2, y:0.8}}>
+                  <Image style={{marginTop:2}} source={require('../../assets/icons/chat.png')}/>
+                </LinearGradient>
+              </Pressable>
+          </View>
+          <LinearGradient colors={['#FCFEFE', '#E2F5F9']} style={styles.card}>
+              <Image style={{position: 'absolute', left: '35%', top:0 }} source={require('../../assets/heartbgpattern.png')} />
+              <View style={{position:'absolute', borderColor:'#10A5C7', borderTopWidth:0, borderBottomWidth:1, borderStyle:'dashed', top:'50%', width:'100%', opacity:0.3}}/>
+              <Image style={{opacity: 0.93, position: 'absolute', alignSelf: 'flex-end', right:16, bottom:0}} source={require('../../assets/avatars/oliver_avatar.png')}/>
+              <Text style={{color: '#374B4C', fontSize: 16, marginTop: 14, marginLeft:18, fontFamily:'Montserrat_700Bold'}}>Dr Oliver SYKES</Text>
+              <Text style={{color: 'rgba(137, 164, 171, 0.9)', fontSize: 12, marginTop: 8, marginLeft:18, fontFamily:'Montserrat_600SemiBold'}}>HEAR SURGEON</Text>
+              <View style={{flexDirection: 'row', alignItems: 'center', columnGap: 8, marginTop: 14, backgroundColor:'#00000000', marginLeft:18}}>
+                  <Ratings rating={5}/>
+                  <Text style={{color: 'rgba(137, 164, 171, 0.9)', fontSize: 10, fontFamily:'Montserrat_500Medium'}}>(22 Reviews)</Text>
+              </View>
+              <Image style={{marginTop:33, marginLeft:18}} source={require('../../assets/mayo_blason.png')}/>
+              <View style={{flexDirection:'row', backgroundColor:'#00000000', marginTop:14, marginLeft:18, alignItems:'center'}}>
+                  <Image source={require('../../assets/icons/localisation_icon.png')}/>
+                  <View style={{flexDirection:'column', marginLeft:16, backgroundColor:'#00000000'}}>
+                      <Text style={{fontSize:10, color:'#89A4AB', fontFamily:'Montserrat_500Medium'}}>9500 Euclid Avenue,</Text>
+                      <Text style={{fontSize:10, color:'#89A4AB', fontFamily:'Montserrat_500Medium'}}>Cleveland, OH</Text>
+                  </View>
+              </View>
+          </LinearGradient>
+        </View>
+        <View style={{flexDirection:'row', marginTop:10, justifyContent:'space-between', alignItems:'baseline', paddingHorizontal:16, backgroundColor:'#00000000'}}>
+            <Text style={{color:'#415556', fontSize:20, fontFamily:'Montserrat_700Bold'}}>Make an Appointment</Text>
+        </View>
+
+        <View style={{height:92, marginTop:20, overflow:'visible', backgroundColor:'#00000000'}}>
+          <ScrollView ref={scrollViewDate} horizontal={true} contentContainerStyle={{height:92, overflow:'visible', columnGap:16, paddingHorizontal:16, }}>
+              {Array.from({ length: 31 }, (_, index) => (<DateButton key={'dbkey'+index} screenWidth={screenWidth} scrollView={scrollViewDate} activeDate={activeDate} setActiveDate={setActiveDate} month={currentMonth} day={index+1} /*active={index+1 === activeDate.day && currentMonth === activeDate.month ? true : false}*//>))}
+          </ScrollView>
+        </View>
+
+        <View style={{height:56, marginTop:5, overflow:'visible', backgroundColor:'#00000000'}}>
+          <ScrollView ref={scrollViewTimeSlot} horizontal={true} contentContainerStyle={{height:56, overflow:'visible', columnGap:16, paddingHorizontal:16, }}>
+              {Timeslots.map((slot, index) => (<TimeSlotButton activeTimeSlot={activeTimeSlot} timeSlotsList={Timeslots} scrollView={scrollViewTimeSlot} setActiveTimeSlot={setActiveTimeSlot} key={'tskey'+index} slot={slot}/>))}
+          </ScrollView>
+        </View>
+
+        <TextInput style={styles.bookingMessageInput} multiline={true} numberOfLines={8} onChangeText={(text) => setBookingMessage(text)} value={bookingMessage} maxLength={600}/>
+
+        <Pressable style={styles.bookingButton}>
+              <LinearGradient style={styles.bookingButtonGradient} colors={['#6BD3EB', '#0FACD0']}>
+                <Text style={{fontSize: 12, color: '#fff', fontFamily:'Montserrat_700Bold', marginTop:-1}}>Book this Appointment</Text>
               </LinearGradient>
-            </View>
-            <Pressable style={styles.blueButtons}>
-              <LinearGradient style={{width:42, height:42, justifyContent:'center', alignItems:'center'}} colors={['#ffffff44', 'transparent']} start={{x:1, y:0}} end={{x:0.2, y:0.8}}>
-                <Image style={{marginTop:2}} source={require('../../assets/icons/chat.png')}/>
-              </LinearGradient>
-            </Pressable>
+        </Pressable>
+
+        <View style={{flexDirection:'row', marginTop:35, justifyContent:'space-between', alignItems:'baseline', paddingHorizontal:16, backgroundColor:'#00000000'}}>
+            <Text style={{color:'#415556', fontSize:20, fontFamily:'Montserrat_700Bold'}}>Your Documents</Text>
+            <Text style={{color:'#29AAC9', fontSize:14, opacity:0.9, textDecorationStyle:'solid', textDecorationLine:'underline', fontFamily:'Montserrat_600SemiBold'}}>See More</Text>
         </View>
-        <LinearGradient colors={['#FCFEFE', '#E2F5F9']} style={styles.card}>
-            <Image style={{position: 'absolute', left: '35%', top:0 }} source={require('../../assets/heartbgpattern.png')} />
-            <View style={{position:'absolute', borderColor:'#10A5C7', borderTopWidth:0, borderBottomWidth:1, borderStyle:'dashed', top:'50%', width:'100%', opacity:0.3}}/>
-            <Image style={{opacity: 0.93, position: 'absolute', alignSelf: 'flex-end', right:16, bottom:0}} source={require('../../assets/avatars/oliver_avatar.png')}/>
-            <Text style={{color: '#374B4C', fontSize: 16, marginTop: 14, marginLeft:18, fontFamily:'Montserrat_700Bold'}}>Dr Oliver SYKES</Text>
-            <Text style={{color: 'rgba(137, 164, 171, 0.9)', fontSize: 12, marginTop: 8, marginLeft:18, fontFamily:'Montserrat_600SemiBold'}}>HEAR SURGEON</Text>
-            <View style={{flexDirection: 'row', alignItems: 'center', columnGap: 8, marginTop: 14, backgroundColor:'#00000000', marginLeft:18}}>
-                <Ratings rating={5}/>
-                <Text style={{color: 'rgba(137, 164, 171, 0.9)', fontSize: 10, fontFamily:'Montserrat_500Medium'}}>(22 Reviews)</Text>
-            </View>
-            <Image style={{marginTop:33, marginLeft:18}} source={require('../../assets/mayo_blason.png')}/>
-            <View style={{flexDirection:'row', backgroundColor:'#00000000', marginTop:14, marginLeft:18, alignItems:'center'}}>
-                <Image source={require('../../assets/icons/localisation_icon.png')}/>
-                <View style={{flexDirection:'column', marginLeft:16, backgroundColor:'#00000000'}}>
-                    <Text style={{fontSize:10, color:'#89A4AB', fontFamily:'Montserrat_500Medium'}}>9500 Euclid Avenue,</Text>
-                    <Text style={{fontSize:10, color:'#89A4AB', fontFamily:'Montserrat_500Medium'}}>Cleveland, OH</Text>
-                </View>
-            </View>
-        </LinearGradient>
-      </View>
-      <View style={{flexDirection:'row', marginTop:10, justifyContent:'space-between', alignItems:'baseline', paddingHorizontal:16, backgroundColor:'#00000000'}}>
-          <Text style={{color:'#415556', fontSize:20, fontFamily:'Montserrat_700Bold'}}>Make an Appointment</Text>
-      </View>
 
-      <View style={{height:92, marginTop:20, overflow:'visible', backgroundColor:'#00000000'}}>
-        <ScrollView ref={scrollViewDate} horizontal={true} contentContainerStyle={{height:92, overflow:'visible', columnGap:16, paddingHorizontal:16, }}>
-            {Array.from({ length: 31 }, (_, index) => (<DateButton key={'dbkey'+index} screenWidth={screenWidth} scrollView={scrollViewDate} activeDate={activeDate} setActiveDate={setActiveDate} month={currentMonth} day={index+1} /*active={index+1 === activeDate.day && currentMonth === activeDate.month ? true : false}*//>))}
-        </ScrollView>
-      </View>
-
-      <View style={{height:56, marginTop:5, overflow:'visible', backgroundColor:'#00000000'}}>
-        <ScrollView ref={scrollViewTimeSlot} horizontal={true} contentContainerStyle={{height:56, overflow:'visible', columnGap:16, paddingHorizontal:16, }}>
-            {Timeslots.map((slot, index) => (<TimeSlotButton activeTimeSlot={activeTimeSlot} timeSlotsList={Timeslots} scrollView={scrollViewTimeSlot} setActiveTimeSlot={setActiveTimeSlot} key={'tskey'+index} slot={slot}/>))}
-        </ScrollView>
-      </View>
-
-      <TextInput style={styles.bookingMessageInput} multiline={true} numberOfLines={8} onChangeText={(text) => setBookingMessage(text)} value={bookingMessage} maxLength={600}/>
-
-      <Pressable style={styles.bookingButton}>
-            <LinearGradient style={styles.bookingButtonGradient} colors={['#6BD3EB', '#0FACD0']}>
-              <Text style={{fontSize: 12, color: '#fff', fontFamily:'Montserrat_700Bold', marginTop:-1}}>Book this Appointment</Text>
-            </LinearGradient>
-      </Pressable>
-
-      <View style={{flexDirection:'row', marginTop:35, justifyContent:'space-between', alignItems:'baseline', paddingHorizontal:16, backgroundColor:'#00000000'}}>
-          <Text style={{color:'#415556', fontSize:20, fontFamily:'Montserrat_700Bold'}}>Your Documents</Text>
-          <Text style={{color:'#29AAC9', fontSize:14, opacity:0.9, textDecorationStyle:'solid', textDecorationLine:'underline', fontFamily:'Montserrat_600SemiBold'}}>See More</Text>
-      </View>
-
-      <View style={styles.docRow}>
-        <Image style={styles.docRowIcon} source={require('../../assets/icons/phone.png')}/>
-        <View style={styles.docRowDatasContainer}>
-          <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>12-07-2020</Text>
-          <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>COVID-19 Prescription</Text>
-          <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>--</Text>
+        <View style={styles.docRow}>
+          <Image style={styles.docRowIcon} source={require('../../assets/icons/phone.png')}/>
+          <View style={styles.docRowDatasContainer}>
+            <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>12-07-2020</Text>
+            <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>COVID-19 Prescription</Text>
+            <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>--</Text>
+          </View>
         </View>
-      </View>
 
-      <View style={styles.docRow}>
-        <Image style={styles.docRowIcon} source={require('../../assets/icons/phone.png')}/>
-        <View style={styles.docRowDatasContainer}>
-          <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>12-07-2020</Text>
-          <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>COVID-19 Prescription</Text>
-          <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>--</Text>
+        <View style={styles.docRow}>
+          <Image style={styles.docRowIcon} source={require('../../assets/icons/phone.png')}/>
+          <View style={styles.docRowDatasContainer}>
+            <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>12-07-2020</Text>
+            <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>COVID-19 Prescription</Text>
+            <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>--</Text>
+          </View>
         </View>
-      </View>
 
-      <View style={styles.docRow}>
-        <Image style={styles.docRowIcon} source={require('../../assets/icons/phone.png')}/>
-        <View style={styles.docRowDatasContainer}>
-          <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>12-07-2020</Text>
-          <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>COVID-19 Prescription</Text>
-          <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>--</Text>
+        <View style={styles.docRow}>
+          <Image style={styles.docRowIcon} source={require('../../assets/icons/phone.png')}/>
+          <View style={styles.docRowDatasContainer}>
+            <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>12-07-2020</Text>
+            <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>COVID-19 Prescription</Text>
+            <Text style={{fontSize:12, fontFamily:'Montserrat_500Medium',}}>--</Text>
+          </View>
         </View>
-      </View>
 
-    </LinearGradient>
-    </ScrollView>
+      </LinearGradient>
+    </KeyboardAwareScrollView>
   );
 }
 
@@ -228,8 +230,6 @@ const styles = StyleSheet.create({
     alignItems:'center',
     borderRadius:6,
     backgroundColor:'#ffffffcc',
-    shadowColor:'#23BEE3',
-    elevation:4,
   },
   docRowDatasContainer:{
     height:36,
