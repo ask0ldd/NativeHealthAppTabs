a057fe42cd904d8b99d9aed6355093bd7d844d1c
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-14 19:30:32 +0200

    successful booking header

--- app/modals/successfullbooking.tsx
+++ app/modals/successfullbooking.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 
@@ -8,7 +8,10 @@ const SuccessfullBooking = (props: Props) => {
   return (
     <ScrollView>
         <LinearGradient colors={['#B9EFF3','#EDF5F7']} style={styles.container}>
-            <Text>successfullbooking</Text>
+            <LinearGradient style={styles.header} colors={['#FFFFFF00','#FFFFFF95','#FFFFFF95','#FFFFFF00']} start={{x:0, y:0}} end={{x:1, y:0}} locations={[0,0.2,0.8,1]}>
+                <Image source={require('../../assets/success.png')}/>
+                <Text style={{ fontSize:16, fontFamily:'Montserrat_700Bold', color:'#415556', opacity:0.9}}>Your Appointment is Booked !</Text>
+            </LinearGradient>
         </LinearGradient>
     </ScrollView>
   )
@@ -22,4 +25,14 @@ const styles = StyleSheet.create({
         flexDirection:'column',
         paddingBottom:40  
     },
+    header:{
+        height:162, 
+        marginTop:30, 
+        flexDirection:'column', 
+        alignItems:'center',
+        paddingTop:5,
+        borderTopWidth:1,
+        borderBottomWidth:1,
+        borderBlockColor:'#fff',
+    }
 })
