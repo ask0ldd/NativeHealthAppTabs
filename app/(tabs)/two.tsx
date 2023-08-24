8bfc0dbf116eba5ace72d42bf7d832bb815613b6
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-24 22:25:10 +0200

    appointment tab base

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,31 +1,33 @@
-import { StyleSheet } from 'react-native';
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 
 import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
-  );
+    <ScrollView>
+      <LinearGradient colors={['#B9EFF3','#EDF5F7']} style={styles.container}>
+        <View style={{flexDirection:'column', paddingHorizontal:16, rowGap:20, marginTop:20}}>
+          <View style={{flexDirection:'column', rowGap:10}}>
+              <Text style={{color:'#374B4C',  fontSize:14, fontFamily:'Montserrat_600SemiBold', opacity:0.9}}>Next Appointment :</Text>
+              <View style={{flexDirection:'row', columnGap:20}}>
+                  <View style={{flexDirection:'column', rowGap:6, width:'100%', flex:1, justifyContent:'center', alignItems:'center', borderRadius:6, shadowColor:'#23BEE3', elevation:6, backgroundColor:'#FFFFFFCC'}}>
+                      <Text style={{color:'#374B4C', fontSize:16, fontFamily:'Montserrat_700Bold',}}>Dr Oliver Sykes</Text>
+                      <Text style={{color:'#89A4AB', opacity:0.9,  fontSize:12, fontFamily:'Montserrat_600SemiBold',}}>HEART SURGEON</Text>
+                  </View>
+                  <View style={{borderRadius:6, shadowColor:'#23BEE3', borderWidth:3, borderColor:'#FFFFFFFF', elevation:6, overflow:'hidden'}}><Image source={require('../../assets/avatars/oliver72_avatar.png')}/></View>
+              </View>
+          </View>
+        </View>        
+      </LinearGradient>
+
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    flexDirection:'column',
   },
 });
