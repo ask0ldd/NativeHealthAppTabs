58bef50f25333fbc9588a7020a74e86a29797cae
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-24 15:58:44 +0200

    speciality active grad

--- components/Speciality.tsx
+++ components/Speciality.tsx
@@ -12,7 +12,7 @@ const Speciality = (props: Props) => {
   return (
     <Pressable style={styles.specialityContainer} onPress={() => props.setSpeciality()}>
         <View style={props.active !== false ? styles.specialityActiveButton : styles.specialityButton}>
-            <LinearGradient style={{width:64, height:64, justifyContent:'center', alignItems:'center'}} colors={['#ffffff44', 'transparent']} start={{x:1, y:0}} end={{x:0.2, y:0.8}}>
+            <LinearGradient style={{width:64, height:64, justifyContent:'center', alignItems:'center'}} colors={props.active !== false ? ['#2EA7C3FF', '#417C8AFF'] : ['#ffffff44', 'transparent']} start={{x:1, y:0}} end={{x:0.2, y:0.8}}>
                 <Image style={styles.icon} source={props.specialityIconUri}/>
             </LinearGradient>
         </View>
@@ -42,6 +42,7 @@ const styles = StyleSheet.create({
         borderColor:'#fff',
         shadowColor:'#39C5E6ff',
         elevation:10,
+        overflow:'hidden',
         // transform: [{rotateZ: '30deg'}],
     },
     specialityActiveButton:{
@@ -52,9 +53,11 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         alignItems:'center',
         borderWidth:3,
-        borderColor:'#546666',
+        // borderColor:'#546666',
+        borderColor:'#FFFFFFFF',
         shadowColor:'#39C5E6ff',
         elevation:10,
+        overflow:'hidden',
         // transform: [{rotateZ: '30deg'}],
     },
     icon : {height:64, width:64, zIndex:3},
