9cca6969fe240020d40f11bb0dcfc36dbc369a76
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-01-01 00:32:59 +0100

    emergency call v2 layout

--- app/(tabs)/emergencycall.tsx
+++ app/(tabs)/emergencycall.tsx
@@ -1,15 +1,19 @@
 import { Image, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import { Text, View } from '../../components/Themed';
 
 export default function TabThreeScreen() {
   return (
-    <View style={styles.container}>
+    <LinearGradient colors={['#B9EFF3','#EDF5F7']} style={styles.container}>
       <View style={styles.avatar}>
-        <Image source={require('../../assets/avatars/oliver72_avatar.png')}/>
+        <Image source={require('../../assets/avatars/oliver92_avatar.png')}/>
       </View>
-      <Text style={styles.title}>Dr Oliver SYKES</Text>
-      <Text style={styles.calling}>Calling...</Text>
+      <View style={styles.doctorCallingContainer}>
+        <Text style={styles.title}>Dr Oliver Sykes</Text>
+        <View style={styles.separatorLine}/>
+        <Text style={styles.calling}>Calling...</Text>
+      </View>
       <View style={{...styles.iconsRow, marginTop : 64}}>
           <Image style={{width:64,}} source={require('../../assets/avatars/oliver72_avatar.png')}/>
           <Image style={{width:64,}} source={require('../../assets/avatars/oliver72_avatar.png')}/>
@@ -20,7 +24,7 @@ export default function TabThreeScreen() {
           <Image style={{width:64,}} source={require('../../assets/avatars/oliver72_avatar.png')}/>
           <Image style={{width:64,}} source={require('../../assets/avatars/oliver72_avatar.png')}/>
       </View>
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -34,28 +38,46 @@ const styles = StyleSheet.create({
   avatar : {
     borderRadius:6, 
     shadowColor:'#23BEE3', 
-    borderWidth:3, 
+    borderWidth:4, 
     borderColor:'#FFFFFFFF', 
     elevation:6, 
     overflow:'hidden',
     marginTop:-48,
   },
+  doctorCallingContainer: {
+    flexDirection:'column', 
+    padding:18, 
+    width:'90%', 
+    alignItems:'center', 
+    borderRadius:6, 
+    shadowColor:'#23BEE3', 
+    elevation:6, 
+    backgroundColor:'#FFFFFFCC',
+    marginTop:36,
+    rowGap:10,
+  },
   title: {
-    marginTop : 28,
+    /*marginTop : 28,*/
     fontSize: 24,
     fontWeight: 'bold',
     color:'#415556',
     fontFamily:'Montserrat_700Bold',
   },
+  separatorLine : {
+    width: '80%',
+    height: 1,
+    backgroundColor: '#C0E8EC',
+  },
   calling: {
-    fontSize : 20,
-    marginTop: 12,
+    fontSize : 18,
     color:'#415556',
     fontFamily:'Montserrat_500Medium',
+    marginTop: 2,
   },
   iconsRow: {
     flexDirection: 'row',
     columnGap: 24,
+    backgroundColor: 'transparent',
   },
   iconContainer:{
     width: 64,
