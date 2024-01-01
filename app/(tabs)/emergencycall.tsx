2f5e72b3b1eead29366ef6f9ecb2b85cdb869eec
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-01-01 01:43:52 +0100

    emergency call background waves

--- app/(tabs)/emergencycall.tsx
+++ app/(tabs)/emergencycall.tsx
@@ -2,17 +2,95 @@ import { Image, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { Text, View } from '../../components/Themed';
+import React, { useEffect, useState } from 'react';
+import { transform } from '@babel/core';
 
 export default function TabThreeScreen() {
+
+  const [nDots, setnDots] = useState<string>('.')
+  const [nCircles, setnCircles] = useState<number>(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setnDots((prevDots) => (prevDots.length >= 3 ? '.' : prevDots + '.'))
+      setnCircles((prevCircles) => (prevCircles >= 5 ? 0 : prevCircles + 1))
+    }, 500)
+
+    return () => clearInterval(interval)
+    },
+  [])
+
   return (
     <LinearGradient colors={['#B9EFF3','#EDF5F7']} style={styles.container}>
+
+      {nCircles>4 && <View style={{
+        position:'absolute',
+        top: '50%',
+        left:'50%',
+        transform: [{ translateX: -250 }, { translateY: -250 }],
+        width: 500,
+        height:500,
+        borderRadius:500,
+        borderWidth:2,
+        borderColor:'#8BDCEF50',
+        backgroundColor:'transparent',
+      }}/>} 
+      {nCircles>3 && <View style={{
+        position:'absolute',
+        top: '50%',
+        left:'50%',
+        transform: [{ translateX: -200 }, { translateY: -200 }],
+        width: 400,
+        height:400,
+        borderRadius:400,
+        borderWidth:2,
+        borderColor:'#8BDCEF77',
+        backgroundColor:'transparent',
+      }}/>}      
+      {nCircles>2 && <View style={{
+        position:'absolute',
+        top: '50%',
+        left:'50%',
+        transform: [{ translateX: -150 }, { translateY: -150 }],
+        width: 300,
+        height:300,
+        borderRadius:300,
+        borderWidth:2,
+        borderColor:'#8BDCEF77',
+        backgroundColor:'transparent',
+      }}/>}
+      {nCircles>1 && <View style={{
+        position:'absolute',
+        top: '50%',
+        left:'50%',
+        transform: [{ translateX: -100 }, { translateY: -100 }],
+        width: 200,
+        height:200,
+        borderRadius:200,
+        borderWidth:2,
+        borderColor:'#8BDCEFaa',
+        backgroundColor:'transparent',
+      }}/>}
+      {nCircles>0 && <View style={{
+        position:'absolute',
+        top: '50%',
+        left:'50%',
+        transform: [{ translateX: -50 }, { translateY: -50 }],
+        width: 100,
+        height:100,
+        borderRadius:100,
+        borderWidth:2,
+        borderColor:'#8BDCEFcc',
+        backgroundColor:'transparent',
+      }}/>}
+
       <View style={styles.avatar}>
         <Image source={require('../../assets/avatars/oliver92_avatar.png')}/>
       </View>
       <View style={styles.doctorCallingContainer}>
         <Text style={styles.title}>Dr Oliver Sykes</Text>
         <View style={styles.separatorLine}/>
-        <Text style={styles.calling}>Calling...</Text>
+        <Text style={styles.calling}>Calling{nDots}</Text>
       </View>
       <View style={{...styles.iconsRow, marginTop : 64}}>
           <Image style={{width:64,}} source={require('../../assets/avatars/oliver72_avatar.png')}/>
@@ -62,6 +140,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color:'#415556',
     fontFamily:'Montserrat_700Bold',
+    letterSpacing:0.25,
   },
   separatorLine : {
     width: '80%',
@@ -83,5 +162,4 @@ const styles = StyleSheet.create({
     width: 64,
     height: 64,
   },
-
 });
